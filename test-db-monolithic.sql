4d6100650ef6da569e552cffa31e010480b9af26
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-11-23 17:31:33 +0100

    Increase test queries count

--- test-db-monolithic.sql
+++ test-db-monolithic.sql
@@ -1,7 +1,7 @@
 
 do $$
 begin
-   for counter in 1..1000 loop
+   for counter in 1..1000000 loop
  	update "users" set "lastLoggedAt"=now(), "updatedAt"=now() where id=(select floor (random () * 1000000 + 1) :: int);
 	 -- "perform" is for a "select" when the returned values are not used
  	perform * from "users" where id=(select floor (random () * 1000000 + 1) :: int);
--- test-db-split.sql
+++ test-db-split.sql
@@ -1,7 +1,7 @@
 
 do $$
 begin
-   for counter in 1..1000 loop
+   for counter in 1..1000000 loop
  	update "user-logins" set "lastLoggedAt"=now(), "updatedAt"=now() where "userId"=(select floor (random () * 1000000 + 1) :: int);
       	-- "perform" is for a "select" when the returned values are not used
 	perform * from "users" where id=(select floor (random () * 1000000 + 1) :: int);
