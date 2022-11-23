93219368e72ca4e9ba2c7a643f9cf9902a8a157e
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2022-11-23 09:51:19 +0100

    Add schemas and first iteration of files

---
+++ db-monolithic.sql
@@ -0,0 +1,28 @@
+create table users (
+  id serial primary key,
+  firstName character varying(255) not null,
+  lastName character varying(255) not null,
+  email character varying(255) not null,
+  createdAt timestamp with time zone not null default ('now'::text)::timestamp with time zone,
+  updatedAt timestamp with time zone not null default ('now'::text)::timestamp with time zone,
+  cgu boolean,
+  pixOrgaTermsOfServiceAccepted boolean default false,
+  pixCertifTermsOfServiceAccepted boolean default false,
+  hasSeenAssessmentInstructions boolean default false,
+  username character varying(255) not null,
+  mustValidateTermsOfService boolean not null default false,
+  lastTermsOfServiceValidatedAt timestamp with time zone,
+  lang character varying(255) not null default 'fr'::character varying,
+  hasSeenNewDashboardInfo boolean default false,
+  isAnonymous boolean default false,
+  emailConfirmedAt timestamp with time zone,
+  hasSeenFocusedChallengeTooltip boolean default false,
+  lastLoggedAt timestamp with time zone,
+  hasSeenOtherChallengesTooltip boolean default false,
+  lastPixOrgaTermsOfServiceValidatedAt timestamp with time zone,
+  lastPixCertifTermsOfServiceValidatedAt timestamp with time zone,
+  unique (email),
+  unique (username)
+);
+
+create index "users_email_lower" on "users"(LOWER("email"));
